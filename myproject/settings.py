ee1192ba60362986ac29b20550dbfff615d2cf9a
Mtnikmaram <mtnikmaram@gmail.com>
2023-08-03 01:47:54 +0330

    creating a superuser

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,10 +25,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['art-gallery-5d397039d9e0.herokuapp.com', 'localhost']
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['art-gallery-5d397039d9e0.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 
