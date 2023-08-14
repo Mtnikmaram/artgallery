274b52ca4796f7af4285a400a38f0ef4f036907e
Mtnikmaram <mtnikmaram@gmail.com>
2023-08-14 17:09:00 +0330

    CHANGING THE DEBUG TO FALSE

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 ALLOWED_HOSTS = ['127.0.0.1:8000']
 ALLOWED_HOSTS = ['art-gallery-5d397039d9e0.herokuapp.com', 'localhost', '127.0.0.1']
 
