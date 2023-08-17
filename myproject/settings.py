08515377fd3f05837fe15a081340396903412e94
Mtnikmaram <>
2023-08-17 11:18:18 +0000

    change debug to false

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,11 +25,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 ALLOWED_HOSTS = ['127.0.0.1:8000']
-ALLOWED_HOSTS = ['art-gallery-dbb39ec83ba6.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['art-gallery-dbb39ec83ba6.herokuapp.com', '8000-mtnikmaram-artgallery-xc1l6in19j.us2.codeanyapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
