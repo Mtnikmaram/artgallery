11a46e3dec70ecc8358b55a9fd78ac06fbeb2a63
Mtnikmaram <mtnikmaram@gmail.com>
2023-08-17 12:09:27 +0330

    deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,10 +135,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_STORAGE = 'cloudinary_storage.storage.staticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_STORAGE = 'cloudinary_storage.storage.staticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
