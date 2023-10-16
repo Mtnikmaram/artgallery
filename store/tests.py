162860ee6a791028c3befb86d3b542c307f0b391
Mtnikmaram <mtnikmaram@gmail.com>
2023-10-16 12:00:46 +0330

    adding test.py

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .forms import CommentForm
 
-# Create your tests here.
+
+class TestCommentForm(TestCase):
+    """
+    This function tests if the comment body
+    field is required.
+    """
+
+    def test_comment_body_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
