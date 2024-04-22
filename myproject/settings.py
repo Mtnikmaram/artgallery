312232b36e073fd95386680092e936c7fcce0b56
Mtnikmaram <mtnikmaram@gmail.com>
2024-04-22 13:59:31 +0330

    creating tthe migrations

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 ALLOWED_HOSTS = ['127.0.0.1:8000']
 ALLOWED_HOSTS = ['art-gallery-dbb39ec83ba6.herokuapp.com', 'localhost', '127.0.0.1']
